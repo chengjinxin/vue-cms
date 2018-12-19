c7befc041b889b3bd7264ad18969c9dcfac02ca2
xiaoxin <1374856262@qq.com>
2018-12-19 18:48:07 +0800

    页面基本布局

--- src/components/goods/goodsInfo.vue
+++ src/components/goods/goodsInfo.vue
@@ -6,7 +6,16 @@
         <div class="mui-card-content-inner">这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等</div>
       </div>
     </div>
+
     <!-- 购买区域 -->
+    <div class="mui-card">
+      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+      </div>
+    </div>
+
+    <!-- 商品参数 -->
     <div class="mui-card">
       <div class="mui-card-header">页眉</div>
       <div class="mui-card-content">
@@ -14,7 +23,6 @@
       </div>
       <div class="mui-card-footer">页脚</div>
     </div>
-    <!-- 商品参数 -->
   </div>
 </template>
 
