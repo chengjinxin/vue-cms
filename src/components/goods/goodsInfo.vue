ab6bdfc169ba01149809c557b813a1369639f4e7
xiaoxin <1374856262@qq.com>
2018-12-20 16:14:36 +0800

    页中

--- src/components/goods/goodsInfo.vue
+++ src/components/goods/goodsInfo.vue
@@ -13,7 +13,16 @@
     <div class="mui-card">
       <div class="mui-card-header">商品的名称标题</div>
       <div class="mui-card-content">
-        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+        <div class="mui-card-content-inner">
+          <p class="price">
+            市场价:<del>$2399</del>&nbsp;&nbsp;销售价: <span class="now_price">$2199</span>
+          </p>
+          <p>购买数量:</p>
+          <p>
+            <mt-button type='primary' size='small'>立即购买</mt-button>
+            <mt-button type='danger' size='small'>加入购物车</mt-button>
+          </p>
+        </div>
       </div>
     </div>
 
